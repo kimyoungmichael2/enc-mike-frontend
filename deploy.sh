b44e6147138b04876349e699d95cfec0768fd1c3
Michael Kim <kimyoungmichael2@gmail.com>
2021-03-10 14:12:39 -0500

    small edit to instruction

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#once you have git pull-ed
-#run "sudo bash deploy.sh" to copy the necessary files over to nginx directory and restart nginx
+#once you have git pull-ed, run npm run build
+#then, run "sudo bash deploy.sh" to copy the necessary files over to nginx directory and restart nginx
 
-cp -r ./build /var/www/michaelyoung.kim/encourage
+cp -r ./build/. /var/www/michaelyoung.kim/encourage
 echo copy complete!
 
 systemctl reload nginx
